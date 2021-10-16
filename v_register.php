1095a99c59e2ccace0cc48c1302e0b66edd077b3
yogitaufik <90616708+yogitaufik@users.noreply.github.com>
2021-10-16 16:36:31 +0700

    Create v_register.php

---
+++ v_register.php
@@ -0,0 +1,61 @@
+<div class="form_wrapper">
+  <div class="form_container">
+    <div class="title_container">
+      <h2>Registrasi</h2>
+    </div>
+    <div class="row clearfix">
+      <div class="">
+        <form>
+          <div class="input_field"> <span><i aria-hidden="true" class="fa fa-envelope"></i></span>
+            <input type="email" name="email" placeholder="Email" required />
+          </div>
+          <div class="input_field"> <span><i aria-hidden="true" class="fa fa-lock"></i></span>
+            <input type="password" name="password" placeholder="Password" required />
+          </div>
+          <div class="input_field"> <span><i aria-hidden="true" class="fa fa-lock"></i></span>
+            <input type="password" name="password" placeholder="Ulangi Password" required />
+          </div>
+          <div class="row clearfix">
+            <div class="col_half">
+              <div class="input_field"> <span><i aria-hidden="true" class="fa fa-user"></i></span>
+                <input type="text" name="name" placeholder="Nama Depan" />
+              </div>
+            </div>
+            <div class="col_half">
+              <div class="input_field"> <span><i aria-hidden="true" class="fa fa-user"></i></span>
+                <input type="text" name="name" placeholder="Nama Belakang" required />
+              </div>
+            </div>
+          </div>
+              <div class="input_field radio_option">
+              <input type="radio" name="radiogroup1" id="rd1">
+              <label for="rd1">Pria</label>
+              <input type="radio" name="radiogroup1" id="rd2">
+              <label for="rd2">Wanita</label>
+              </div>
+              <div class="input_field select_option">
+                <select>
+                  <option>Pilih Negara</option>
+                  <option>Afganistan</option>
+                  <option>Bangladesh</option>
+                  <option>China</option>
+                  <option>Denmark</option>
+                  <option>Ekuador</option>
+                  <option>Indonesia</option>
+                </select>
+                <div class="select_arrow"></div>
+              </div>
+            <div class="input_field checkbox_option">
+              <input type="checkbox" id="cb1">
+          <label for="cb1">Saya setuju dengan syarat dan ketentuan</label>
+            </div>
+            <div class="input_field checkbox_option">
+              <input type="checkbox" id="cb2">
+          <label for="cb2">Terima pemberitahuan</label>
+            </div>
+          <input class="button" type="submit" value="Register" />
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
