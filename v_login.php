dcff549b1c72cedf391f363a55f0880e9efa9e52
yogitaufik <90616708+yogitaufik@users.noreply.github.com>
2021-10-16 16:36:03 +0700

    Create v_login.php

---
+++ v_login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>LOG IN</title>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/stylelogin.css"> 
+    </head>
+
+    <body>
+
+      <div class="vid-container">
+        <video id="Video1" class="bgvid back" autoplay="false" muted="muted" preload="auto" loop>
+        <source src="http://shortcodelic1.manuelmasiacsasi.netdna-cdn.com/themes/geode/wp-content/uploads/2014/04/milky-way-river-1280hd.mp4.mp4" type="video/mp4">
+        </video>
+      <div class="inner-container">
+        <video id="Video2" class="bgvid inner" autoplay="false" muted="muted" preload="auto" loop>
+        <source src="http://shortcodelic1.manuelmasiacsasi.netdna-cdn.com/themes/geode/wp-content/uploads/2014/04/milky-way-river-1280hd.mp4.mp4" type="video/mp4">
+        </video>
+      <div class="box">
+        <h1>Login</h1>
+          <input type="text" placeholder="Username"/>
+          <input type="text" placeholder="Password"/>
+          <button>Login</button>
+          <p>Belum Member? <span>Sign Up</span></p>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
